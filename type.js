e89be2d2379d8abc77e6fb82e446c3fd5f4a5e99
manoj <sm727624@gmail.com>
2019-08-25 20:22:56 +0530

    master added file

---
+++ type.js
@@ -0,0 +1,30 @@
+var i = 0;
+var txt = 'Never Give Up..!';
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementsByClassName('js-typewrite')[0].innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 110);
+  }
+}
+
+setTimeout(typeWriter, 2000);
+
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
